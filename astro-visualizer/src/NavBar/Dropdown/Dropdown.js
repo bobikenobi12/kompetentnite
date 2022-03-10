82cc9491abe2faa8efe665e92797f859635160c2
Borislav <borislav1207@gmail.com>
2022-03-10 23:14:50 +0200

    Add NavBar

---
+++ astro-visualizer/src/NavBar/Dropdown/Dropdown.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { useState } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+import "./Dropdown.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+
+export const Dropdown = ({ setSelected }) => {
+  const [isOpen, setOpen] = useState(false);
+  const [clickedOutside, setClickedOutside] = useState(false);
+
+  const myRef = useRef();
+
+  const handleOnClick = (e) => {
+    setOpen(!isOpen);
+    setClickedOutside(false);
+  };
+
+  const handleClickOutside = (e) => {
+    if (!myRef.current.contains(e.target)) {
+      setClickedOutside(true);
+    }
+  };
+  {
+  }
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  });
+  return (
+    <div
+      className={
+        clickedOutside
+          ? "dropdown dropdown--hover"
+          : isOpen
+          ? "dropdown dropdown--click"
+          : "dropdown dropdown--hover"
+      }
+      onClick={(e) => {
+        handleOnClick(e);
+      }}
+      ref={myRef}
+    >
+      <div className="dropdown-title">
+        Algorithms
+        <FontAwesomeIcon className="dropdown-title--icon" icon={faCaretDown} />
+      </div>
+      {clickedOutside ? null : isOpen ? <Items setSelected={setSelected}/> : null}
+    </div>
+  );
+};
+
+const Items = ({ setSelected }) => {
+  const handleOnClick = (e) => {
+    setSelected(e.target.textContent);
+    return null;
+  };
+  return (
+    <div className={"dropdown-items"}>
+      <div
+        className="dropdown-item"
+        onClick={(e) => {
+          handleOnClick(e);
+        }}
+      >
+        Dijkstras
+      </div>
+      <div
+        className="dropdown-item"
+        onClick={(e) => {
+          handleOnClick(e);
+        }}
+      >
+        A*
+      </div>
+      <div
+        className="dropdown-item"
+        onClick={(e) => {
+          handleOnClick(e);
+        }}
+      >
+        Swarm
+      </div>
+    </div>
+  );
+};
---
+++ astro-visualizer/src/NavBar/NavBar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./NavBar.css";
+import { useState } from "react";
+import { Dropdown } from "./Dropdown/Dropdown";
+
+export default function NavBar() {
+  const [selected, setSelected] = useState("Dijkstras");
+  return (
+    <>
+      <div className="controls">
+          <Dropdown className="controls-algorithms" setSelected={setSelected} />
+        <div>
+        <button className="controls-visualize">
+          Start Journey using {selected} algorithm
+        </button>
+        </div>
+        <div>
+        <button className="controls-clear">Clear board</button>
+        </div>
+      </div>
+      <div className="labels">
+        <div className="labels-start">
+          <div className="labels-start--icon"></div>
+          Starting Planet
+        </div>
+        <div className="labels-finish">
+          <div className="labels-finish--icon"></div>
+          Finish Planet
+        </div>
+        <div className="labels-unvisited">
+          <div className="labels-unvisited--icon"></div>
+          Unvisited Node
+        </div>
+        <div className="labels-visited">
+          <div className="labels-visited--icon"></div>
+          Visited Nodes
+        </div>
+        <div className="labels-shortest">
+          <div className="labels-shortest--icon"></div>
+          Shortest-path Node
+        </div>
+        <div className="labels-wall">
+          <div className="labels-wall--icon"></div>
+          Select Space Obstacles
+        </div>
+      </div>
+    </>
+  );
+}
